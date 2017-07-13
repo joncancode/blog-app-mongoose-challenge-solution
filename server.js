da399a1d7ecddb80f6f04ab124d53a4e9059ba4a
joncancode <jc.haines19@gmail.com>
2017-07-13 14:59:25 -0400

    working on /users

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const passport = require('passport')
 
 const { DATABASE_URL, PORT } = require('./config');
-const { BlogPost } = require('./models');
+const { BlogPost, User } = require('./models');
 
 const app = express();
 
@@ -39,6 +40,7 @@ app.get('/posts/:id', (req, res) => {
 });
 
 app.post('/posts', (req, res) => {
+
   const requiredFields = ['title', 'content', 'author'];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -46,8 +48,8 @@ app.post('/posts', (req, res) => {
       const message = `Missing \`${field}\` in request body`
       console.error(message);
       return res.status(400).send(message);
-    }
-  }
+    };
+  };
 
   BlogPost
     .create({
@@ -60,121 +62,140 @@ app.post('/posts', (req, res) => {
       console.error(err);
       res.status(500).json({ error: 'Something went wrong' });
     });
-
 });
 
 app.post('/users', (req, res) => {
-  const requiredFields = ['username', 'password', 'firstName' 'lastName'];
-  for (let i = 0; i < requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  //username is already taken
-  //Adds the user to the database, storing a salted and hashed version of the password
-  //Returns a 201 Created status, with the API representation of the user as the response body
 
+  let { username, password, firstName, lastName } = req.body;
 
+  return User.find({ username }).count().exec()
+    //username is already taken
+    .then(count => {
+      if (count > 0) {
+        return Promise.reject({
+          name: 'AuthenticationError',
+          message: 'Username is already in use. Please choose another.'
 
-  app.delete('/posts/:id', (req, res) => {
-    BlogPost
-      .findByIdAndRemove(req.params.id)
-      .exec()
-      .then(() => {
-        res.status(204).json({ message: 'success' });
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).json({ error: 'something went terribly wrong' });
-      });
-  });
-
-
-  app.put('/posts/:id', (req, res) => {
-    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-      res.status(400).json({
-        error: 'Request path id and request body id values must match'
-      });
-    }
-
-    const updated = {};
-    const updateableFields = ['title', 'content', 'author'];
-    updateableFields.forEach(field => {
-      if (field in req.body) {
-        updated[field] = req.body[field];
+        });
       }
+      return User.hashPassword(password);
+    })
+    //Adds the user to the database, storing a salted and hashed version of the password
+    .then(hash => {
+      return User.create({ username, password: hash, firstName, lastName });
+    })
+    //Returns a 201 Created status, with the API representation of the user as the response body
+    .then(user => {
+      return res.status(201).json(user.apiRepr());
+    })
+    //QUESTION.. why is the catch needed if we have the promise.reject above?
+    .catch(err => {
+      if (err.name === 'AuthenticationError') {
+        return res.status(422).json({ message: err.message });
+      }
+      res.status(500).json({ message: 'Internal server error' });
     });
+});
 
-    BlogPost
-      .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
-      .exec()
-      .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
-      .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+
+
+
+app.delete('/posts/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => {
+      res.status(204).json({ message: 'success' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
+    });
+});
+
+
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+
+  const updated = {};
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
   });
 
+  BlogPost
+    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+    .exec()
+    .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
+    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+});
 
-  app.delete('/:id', (req, res) => {
-    BlogPosts
-      .findByIdAndRemove(req.params.id)
-      .exec()
-      .then(() => {
-        console.log(`Deleted blog post with id \`${req.params.ID}\``);
-        res.status(204).end();
-      });
+
+app.delete('/:id', (req, res) => {
+  BlogPosts
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => {
+      console.log(`Deleted blog post with id \`${req.params.ID}\``);
+      res.status(204).end();
+    });
+});
+
+
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
   });
+}
 
-
-  app.use('*', function (req, res) {
-    res.status(404).json({ message: 'Not Found' });
-  });
-
-  // closeServer needs access to a server object, but that only
-  // gets created when `runServer` runs, so we declare `server` here
-  // and then assign a value to it in run
-  let server;
-
-  // this function connects to our database, then starts the server
-  function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-      mongoose.connect(databaseUrl, err => {
+      console.log('Closing server');
+      server.close(err => {
         if (err) {
           return reject(err);
         }
-        server = app.listen(port, () => {
-          console.log(`Your app is listening on port ${port}`);
-          resolve();
-        })
-          .on('error', err => {
-            mongoose.disconnect();
-            reject(err);
-          });
+        resolve();
       });
     });
-  }
+  });
+}
 
-  // this function closes the server, and returns a promise. we'll
-  // use it in our integration tests later.
-  function closeServer() {
-    return mongoose.disconnect().then(() => {
-      return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
-          if (err) {
-            return reject(err);
-          }
-          resolve();
-        });
-      });
-    });
-  }
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
 
-  // if server.js is called directly (aka, with `node server.js`), this block
-  // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
-  if (require.main === module) {
-    runServer().catch(err => console.error(err));
-  };
-
-  module.exports = { runServer, app, closeServer };
+module.exports = { runServer, app, closeServer };
