5fe7406f5e3e19126b12b757dacc950e45b66646
joncancode <jc.haines19@gmail.com>
2017-07-13 12:43:56 -0400

    added missing fields 400 status

--- models.js
+++ models.js
@@ -5,17 +5,39 @@ const blogPostSchema = mongoose.Schema({
     firstName: String,
     lastName: String
   },
-  title: {type: String, required: true},
-  content: {type: String},
-  created: {type: Date, default: Date.now}
+  title: { type: String, required: true },
+  content: { type: String },
+  created: { type: Date, default: Date.now }
 });
 
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true }
+})
 
-blogPostSchema.virtual('authorName').get(function() {
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt
+  .has(password, 10)
+}
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt
+  .compare(password, this.password)
+}
+
+userSchema.methods.apiRepr = function() {
+username: this.username
+firstName: this.firstName
+lastName: this.lastName
+}
+
+blogPostSchema.virtual('authorName').get(function () {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
-blogPostSchema.methods.apiRepr = function() {
+blogPostSchema.methods.apiRepr = function () {
   return {
     id: this._id,
     author: this.authorName,
@@ -26,5 +48,6 @@ blogPostSchema.methods.apiRepr = function() {
 }
 
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+const User = mongoose.model('User', userSchema);
 
-module.exports = {BlogPost};
+module.exports = { BlogPost, User };
